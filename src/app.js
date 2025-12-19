d222321f4b6dc9153e2b90b21f3c0d318c63a1a6
kristoferide-coder <kristoferide@gmail.com>
2025-12-18 21:59:18 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Vitalux funcionando 🚀</h1>
+      <p>Si ves esto, el deploy resultó.</p>
+    </div>
+  );
+}
+
+export default App;
