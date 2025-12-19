ba89024c9c8dbef2c8fe8d2514d305edb85a950e
kristoferide-coder <kristoferide@gmail.com>
2025-12-18 22:42:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Vitalux funcionando 🚀</h1>
+      <h1>Vitalux funcionando</h1>
       <p>Si ves esto, el deploy resultó.</p>
     </div>
   );
