d083445e83469d4f21c881462f34490e010c86ed
kristoferide-coder <kristoferide@gmail.com>
2025-12-20 18:35:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
   return (
-    
+    <div className="app">
       {/* Aquí va todo tu contenido */}
+      <h1>VitaluxFit</h1>
+      <p>Tu vitalidad, nuestro compromiso.</p>
     </div>
   );
 }
